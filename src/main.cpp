718233b06726be30f447bd354f2974cd66b1df8a
david cramer <david@erraticsoft.com>
2023-06-22 22:41:06 -0500

    basic subsystems and render test

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ TODO: #4 create a screen renderer         []
 
 
 #include <iostream>
+#include <string>
 #include <fstream>
 
 #include "SDL2/SDL.h"
@@ -51,8 +52,53 @@ TODO: #4 create a screen renderer         []
 
 
 
+
+// Globals --Don't love this but here for now
+SDL_Window *win = 0;
+SDL_Renderer *render = 0; // Note: Why are we setting this to 0? Error checking? NULL works for that too
+                          // --Keep an eye on this--
+
+
+
+
 int init() {
   std::cout << "init() Called!\n";
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+
+    win = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+    std::cout << "\nChecking systems we care about...\n";
+
+    if(SDL_WasInit(SDL_INIT_VIDEO) != 0) {
+
+      std::cout << "\tVideo initialized! \n";
+    }
+    
+    if(SDL_WasInit(SDL_INIT_AUDIO) != 0) {
+
+      std::cout << "\tAudio initialized! \n";
+    }
+
+    if(SDL_WasInit(SDL_INIT_TIMER) != 0) {
+
+      std::cout << "\tTimer initialized! \n\n";
+    }
+
+
+
+
+
+
+    if (win != 0) {
+      
+      render = SDL_CreateRenderer(win, -1, 0);
+      // **Add error checking here!**
+    }
+  } 
+  else {
+    std::cout << "ERROR: " << SDL_GetError() << std::endl;
+  }
 
 return 0;
 }
@@ -60,30 +106,47 @@ return 0;
 int load() {
   std::cout << "load() Called!\n";
 
-  int init_status = SDL_Init(SDL_INIT_EVERYTHING);
-
-  std::cout << "\tSDL_Init status: " + init_status << std::endl;
-
 return 0;
 }
 
 int close() {
   std::cout << "close() Called!\n";
+
+  // Free everything like the good programmer we aren't
+  win = 0;
+  render = 0;
  
+
+  // Quit all extensions
   TTF_Quit();
   Mix_Quit();
   IMG_Quit();
-  
+
+  // Quit SDL
   SDL_Quit(); 
 
 return 0;
 }
 
-int main(int args, char *argv[]) {
+int main(int argc, char *args[]) {
 
   init();
   load();
   
+  // Everything logic goes between here and close();
+
+
+  // simple render test
+  SDL_SetRenderDrawColor(render, 0, 255, 0, 255);
+  SDL_RenderClear(render);
+  SDL_RenderPresent(render);
+  SDL_Delay(2000);
+
+  // SMTPE Texture render test
+
+
+
+
   
   
   
